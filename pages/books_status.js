44758bdd15eab223a29800b7dbc8752a5502c6a7
deshpandeadi1901 <deshpande.aditya@northeastern.edu>
2024-03-18 16:51:26 -0400

    in class work

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,10 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+
+exports.show_all_books_status = async (res) =>  {
+  BookInstance.find({status: "Available"}).populate('book').then(list_bookInstances => {
+    res.send(list_bookInstances.map((instance) => {
+      return instance.book.title + " : " + instance.status;
+    }))
+  });
 }
